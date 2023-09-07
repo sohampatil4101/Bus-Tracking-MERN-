d93a4f5c296d00b0d528f39f5b1e38337940ed6f
Darshan <darshanpatel2923@gmail.com>
2023-09-07 22:54:20 +0530

    Adjustments in footer left rest layout is done

--- Frontend/bustrackingapp/src/pages/Home.js
+++ Frontend/bustrackingapp/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <Footer />
         </>
     )
 }
