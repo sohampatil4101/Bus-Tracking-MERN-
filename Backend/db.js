446d9248b913ac86f1d68e1110789fa177d61a9e
Darshan <darshanpatel2923@gmail.com>
2023-10-04 21:20:06 +0530

    syncing 2 repos

--- Backend/db.js
+++ Backend/db.js
@@ -1,15 +1,16 @@
-const mongoose =require("mongoose")
-const connectToMongo = ()=>{ mongoose.connect("mongodb://0.0.0.0:27017/bustracking",{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
-.then(()=>{
-    console.log("mongodb connected successfully!!!");          
-})
-.catch((e)=>{
-    console.log("mongo Failed");
-    console.log("Error is:",e, "soham");
-})
+const mongoose = require("mongoose")
+const connectToMongo = () => {
+    mongoose.connect("mongodb://0.0.0.0:27017/bustracking", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("mongodb connected successfully!!!");
+    })
+    .catch((e) => {
+        console.log("mongo Failed");
+        console.log("Error is:", e, "darshan");
+    })
 }
 
 
